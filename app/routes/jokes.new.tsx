2bacd69270fb7f3546805e0db1c39aef3f78ce42
Bruno Silva <bruno.silva@seedz.ag>
2023-10-10 11:53:30 -0300

    feat: new Joke

--- app/routes/jokes.new.tsx
+++ app/routes/jokes.new.tsx
@@ -1,10 +1,40 @@
+import { redirect, type ActionFunctionArgs } from "@remix-run/node";
+import { createJoke } from "~/models/joke.server";
+
+export const action = async (params: ActionFunctionArgs) => {
+  const formData = await params.request.formData();
+  const name = formData.get("name");
+  const content = formData.get("content");
+
+  if (typeof name !== "string" || typeof content !== "string") {
+    throw new Error("invalid");
+  }
+
+  const newJoke = await createJoke({ name, content });
+  return redirect(`/jokes/${newJoke.id}`);
+};
 function NewJoke() {
   return (
-    <form>
-      <input type="text" name="name" />
-      <textarea name="content" />
-      <button type="submit">Add</button>
-    </form>
+    <section>
+      <p>Add your own hilarious joke</p>
+      <form method="post">
+        <div>
+          <label>
+            Name: <input type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Content: <textarea name="content" />
+          </label>
+        </div>
+        <div>
+          <button type="submit" className="button">
+            Add
+          </button>
+        </div>
+      </form>
+    </section>
   );
 }
 

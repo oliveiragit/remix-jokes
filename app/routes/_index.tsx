82c706ea070803f08c4a6fef45598089f490f2dd
Bruno Silva <bruno.silva@seedz.ag>
2023-10-10 10:03:41 -0300

    feat: add className css

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { LinksFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 import stylesUrl from "~/styles/index.css";
 
@@ -7,7 +8,22 @@ export const links: LinksFunction = () => [
 ];
 
 function IndexRoute() {
-  return <span>Index Route</span>;
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default IndexRoute;
